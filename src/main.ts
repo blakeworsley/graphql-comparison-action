0f09587f4d235c850c8728b3fecd46f78f9bae31
Blake Worsley <blake.worsley@guildeducation.com>
2020-10-08 17:04:37 -0600

    trying some new things

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,32 @@
 import * as core from '@actions/core'
+import {buildSchema} from 'graphql'
+import * as fs from 'fs'
+import * as typescriptPlugin from '@graphql-codegen/typescript'
+import {codegen} from '@graphql-codegen/core'
+
+import {printSchema, parse, GraphQLSchema} from 'graphql'
+import {makeExecutableSchema} from '@graphql-tools/schema'
+
+const schema: GraphQLSchema = buildSchema(`type A {}`)
+const outputFile = 'relative/pathTo/filename.ts'
+
+const schemaA = makeExecutableSchema({typeDefs})
+
+const config = {
+  // used by a plugin internally, although the 'typescript' plugin currently
+  // returns the string output, rather than writing to a file
+  filename: outputFile,
+  schema: parse(printSchema(schema)),
+  plugins: [
+    // Each plugin should be an object
+    {
+      typescript: {} // Here you can pass configuration to the plugin
+    }
+  ],
+  pluginMap: {
+    typescript: typescriptPlugin
+  }
+}
 
 async function run(): Promise<void> {
   try {
@@ -7,6 +35,7 @@ async function run(): Promise<void> {
     const remoteSchemaAuthToken: string = core.getInput(
       'remote-schema-auth_token'
     )
+
     core.debug(`localSchemaPath -- ${localSchemaPath}`)
     core.debug(`remoteSchemaEndpoint -- ${remoteSchemaEndpoint}`)
     core.debug(`remoteSchemaAuthToken -- ${remoteSchemaAuthToken}`)
